d51a9ec88220f5d7485a66392487517b91011c36
Phil Howard <phil@gadgetoid.com>
2024-02-20 16:36:47 +0000

    Swap smbus for smbus2.

--- cap1xxx/__init__.py
+++ cap1xxx/__init__.py
@@ -10,15 +10,8 @@ CAP1166 - 6 Inputs, 6 LEDs
 import atexit
 import threading
 import time
-from sys import version_info
 
-try:
-    from smbus import SMBus
-except ImportError:
-    if version_info[0] < 3:
-        raise ImportError("This library requires python-smbus\nInstall with: sudo apt-get install python-smbus")
-    elif version_info[0] == 3:
-        raise ImportError("This library requires python3-smbus\nInstall with: sudo apt-get install python3-smbus")
+from smbus2 import SMBus
 
 try:
     import RPi.GPIO as GPIO
